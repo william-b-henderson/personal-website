db48f26398d6fd39112965e312b64f29c95a389b
william-b-henderson <william_henderson@berkeley.edu>
2022-04-22 14:24:31 -0700

    Update ProjectCards to link on hover, remove green button from them

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-import { GreenButton } from '../Buttons';
+import Link from 'next/link';
+// import { GreenButton } from '../Buttons';
 import ProjectCardTitleBar from './ProjectCardTitleBar';
 
 interface ProjectCardProps {
@@ -14,27 +15,31 @@ interface ProjectCardProps {
 
 export const ProjectCard = ({ title, description, imageAddress, link, language, year }: ProjectCardProps) => {
   return (
-    <Flex
-      direction="column"
-      justify="space-between"
-      width="45%"
-      maxWidth="30rem"
-      height="30em"
-      bg="secondary.dorian"
-      m="10px"
-      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
-      borderRadius="md"
-      position="relative"
-    >
-      <ProjectCardTitleBar title={title} language={language} year={year} />
-      <Image src={imageAddress} alt={title} layout="responsive" width="3000px" height="1500px" />
-      <Box w="100%" h="10em" overflow="scroll" px="1rem">
-        <Text textAlign="center" textStyle="sec3" mt="10px">
-          {description}
-        </Text>
-      </Box>
-
-      <GreenButton link={link} linkText="View Project" />
-    </Flex>
+    <Link href={link} passHref>
+      <Flex
+        direction="column"
+        justify="flex-start"
+        width="45%"
+        maxWidth="30rem"
+        height="30em"
+        bg="secondary.dorian"
+        m="10px"
+        boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
+        borderRadius="md"
+        position="relative"
+        _hover={{
+          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 30px',
+          cursor: 'pointer',
+        }}
+      >
+        <ProjectCardTitleBar title={title} language={language} year={year} />
+        <Image src={imageAddress} alt={title} layout="responsive" width="3000px" height="1500px" />
+        <Flex w="100%" px="1rem">
+          <Text textAlign="center" textStyle="sec3" mt="10px">
+            {description}
+          </Text>
+        </Flex>
+      </Flex>
+    </Link>
   );
 };
--- components/ProjectCard/ProjectCardOneLiner.tsx
+++ components/ProjectCard/ProjectCardOneLiner.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-import { GreenButton } from '../Buttons';
+import Link from 'next/link';
+// import { GreenButton } from '../Buttons';
 import ProjectCardTitleBar from './ProjectCardTitleBar';
 
 interface ProjectCardProps {
@@ -14,27 +15,33 @@ interface ProjectCardProps {
 
 export const ProjectCardOneLiner = ({ title, oneLiner, imageAddress, link, language, year }: ProjectCardProps) => {
   return (
-    <Flex
-      direction="column"
-      justify="space-between"
-      width="30%"
-      maxWidth="30rem"
-      height="20rem"
-      bg="secondary.dorian"
-      m="10px"
-      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
-      borderRadius="md"
-      overflow={'hidden'}
-    >
-      <ProjectCardTitleBar title={title} language={language} year={year} />
-      <Image src={imageAddress} alt={title} layout="responsive" width="3000px" height="1500px" />
-      <Box w="100%" h="3.5em" overflow="scroll">
-        <Text textAlign="center" textStyle="sec3">
-          {oneLiner}
-        </Text>
-      </Box>
+    <Link href={link} passHref>
+      <Flex
+        direction="column"
+        justify="flex-start"
+        width="30%"
+        maxWidth="30rem"
+        height="20rem"
+        bg="secondary.dorian"
+        m="10px"
+        boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
+        borderRadius="md"
+        overflow={'hidden'}
+        _hover={{
+          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 30px',
+          cursor: 'pointer',
+        }}
+      >
+        <ProjectCardTitleBar title={title} language={language} year={year} />
+        <Image src={imageAddress} alt={title} layout="responsive" width="3000px" height="1500px" />
+        <Box w="100%" h="3.5em" overflow="scroll">
+          <Text textAlign="center" textStyle="sec3">
+            {oneLiner}
+          </Text>
+        </Box>
 
-      <GreenButton link={link} linkText="View Project" />
-    </Flex>
+        {/* <GreenButton link={link} linkText="View Project" /> */}
+      </Flex>
+    </Link>
   );
 };
